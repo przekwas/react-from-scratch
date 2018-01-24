fad9acb999f77b35f53329848a42f1ffa5442e53
Luke Przekwas <galactica@The-Galactica.local>
2018-01-24 14:53:59 -0600

    working react app made from scratch

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+    render () {
+        return <h1 className="display-1 m-5" >It's working!!</h1>
+    }
+}
+
+export default App;
